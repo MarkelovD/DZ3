bbe1148689b665b53e3cf6c03aeb07f7052c2977
Dmitry <markelov16@gmail.com>
2022-10-08 02:25:40 +0500

    задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задача 1
+Console.WriteLine("введите пятизначное число");
+int num = int.Parse(Console.ReadLine()!);
+if ((num%10)==(num/10000) && (num/10%10) == (num/1000%10)){
+    Console.WriteLine($"число {num} является палиндромом");
+}
+else{
+    Console.WriteLine($"число {num} не является палиндромом");
+}
