26cf7e635e2d8e52be712c81615a42cac4d2ed64
Dmitry <markelov16@gmail.com>
2022-10-08 03:01:38 +0500

    задача 3

--- Program.cs
+++ Program.cs
@@ -9,11 +9,20 @@
 // }
 
 //задача 2
-int a_x = int.Parse(Console.ReadLine()!);
-int a_y = int.Parse(Console.ReadLine()!);
-int a_z = int.Parse(Console.ReadLine()!);
-int b_x = int.Parse(Console.ReadLine()!);
-int b_y = int.Parse(Console.ReadLine()!);
-int b_z = int.Parse(Console.ReadLine()!);
-double rast = Math.Sqrt(Math.Pow(b_x-a_x,2)+Math.Pow(b_y-a_y,2)+Math.Pow(b_z-a_z,2));
-Console.WriteLine($"расстояние между 2мя точками в 3D пространстве равно {Math.Round(rast,2)}");
+// int a_x = int.Parse(Console.ReadLine()!);
+// int a_y = int.Parse(Console.ReadLine()!);
+// int a_z = int.Parse(Console.ReadLine()!);
+// int b_x = int.Parse(Console.ReadLine()!);
+// int b_y = int.Parse(Console.ReadLine()!);
+// int b_z = int.Parse(Console.ReadLine()!);
+// double rast = Math.Sqrt(Math.Pow(b_x-a_x,2)+Math.Pow(b_y-a_y,2)+Math.Pow(b_z-a_z,2));
+// Console.WriteLine($"расстояние между 2мя точками в 3D пространстве равно {Math.Round(rast,2)}");
+
+//задача 3
+Console.WriteLine("введите число число");
+int num = int.Parse(Console.ReadLine()!);
+int i = 1;
+while (i<=num){
+Console.WriteLine($"куб числа {i} равен {Math.Pow(i,3)}");
+i++;
+}
